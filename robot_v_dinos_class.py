984441ce09170fed67be7054a28c7f8f82895f3a
TerryW624 <terrywade634@gmail.com>
2023-05-30 15:06:44 -0700

    bug: inf miss action feat: attack enemy choice

--- robot_v_dinos_class.py
+++ robot_v_dinos_class.py
@@ -13,7 +13,7 @@ class Robot:
     
     def attack(self, dinosaur):
         weapon = input(f'''
- stands before you. What weapon do you use?
+{dinosaur.name} stands before you. What weapon do you use?
 (1) {self.active_weapons[0].name}
 (2) {self.active_weapons[1].name}
 (3) {self.active_weapons[2].name}
@@ -30,7 +30,9 @@ class Robot:
 
 class Fleet:
     def __init__(self, bot_one, bot_two, bot_three):
-        self.fleet = [bot_one, bot_two, bot_three]
+        self.name = "Bots"
+        self.group = [bot_one, bot_two, bot_three]
+        self.health = bot_one.health + bot_two.health + bot_three.health
 
 class Dinosaur:
     def __init__(self, name, ability_one, ability_two, ability_three):
@@ -40,7 +42,7 @@ class Dinosaur:
 
     def attack(self, robot):
         ability = input(f'''
- stands before you. What ability do you use?
+{robot.name} stands before you. What ability do you use?
 (1) {self.attack_powers[0].name}
 (2) {self.attack_powers[1].name}
 (3) {self.attack_powers[2].name}
@@ -57,73 +59,140 @@ class Dinosaur:
 
 class Herd:
     def __init__(self, dino_one, dino_two, dino_three):
-        self.herd = [dino_one, dino_two, dino_three]
+        self.name = "Dinos"
+        self.group = [dino_one, dino_two, dino_three]
+        self.health = dino_one.health + dino_two.health + dino_three.health
     
 class Battlefield:
-    def __init__(self, robot, dinosaur):
-        self.robot = robot
-        self.dinosaur = dinosaur
+    def __init__(self, fleet, herd):
+        self.fleet = fleet
+        self.herd = herd
+    
+    def player_set_up(self):
+        self.p1_group = self.choose_team()
+        if self.p1_group == self.fleet:
+            self.p2_group = self.herd
+            print(f"Player 1 you are {self.fleet.name}!")
+            print(f"Player 2 you are {self.herd.name}!")
+        elif self.p1_group == self.herd:
+            self.p2_group = self.fleet
+            print(f"Player 1 you are {self.herd.name}!")
+            print(f"Player 2 you are {self.fleet.name}!")
 
     def display_welcome(self):
         print('''
         Welcome to Bots Vs Dinos!
         ''')
 
+    
     def choose_team(self):
-        team = input("Will you be a (1)Bot? OR A (2)Dino?")
+        team = input('''
+Player 1 choose your side of the battlefield. (1)Bots? OR (2)Dinos?
+    ''')
         if team == "1":
-            return self.robot
+            return self.fleet
         elif team == "2":
-            return self.dinosaur
+            return self.herd
+        
+    def p1_select_an_attack(self):
+        groups = [self.p1_group.group, self.p2_group.group]
+        attack_selection = input(f'''
+Select your enemy to attack 
+(1) {groups[1][0].name}
+(2) {groups[1][1].name}
+(3) {groups[1][2].name}
+''')
+        return attack_selection
+    
+    def p2_select_an_attack(self):
+        groups = [self.p1_group.group, self.p2_group.group]
+        attack_selection = input(f'''
+Select your enemy to attack 
+(1) {groups[0][0].name}
+(2) {groups[0][1].name}
+(3) {groups[0][2].name}
+''')
+        return attack_selection
+
+    def choose_enemy_hit(self):
+        groups = [self.p1_group.group, self.p2_group.group]
+        for group in groups:
+            for dinobot in group:
+                hit_or_miss_list = ["dino or bot", "dino or bot", "Missed", "dino or bot ", "Missed", "dino or bot", "Missed", "dino or bot", "dino or bot", "Missed"]
+                attack_succeed_or_fail = choice(hit_or_miss_list)
+                attack_attempt = False
+                while attack_attempt == False:
+                    if group == groups[0]:
+                        if attack_succeed_or_fail == "dino or bot":
+                            attack_selection = self.p1_select_an_attack()
+                            if attack_selection == "1":
+                                if groups[1][0].health > 0:
+                                    dinobot.attack(groups[1][0])
+                                    attack_attempt = True
+                                else:
+                                    print(f"{groups[1][0].name} is dead!")
+                                    attack_selection = self.p1_select_an_attack()
+                            elif attack_selection == "2":
+                                if groups[1][1].health > 0:
+                                    dinobot.attack(groups[1][1])
+                                    attack_attempt = True
+                                else:
+                                    print(f"{groups[1][1].name} is dead!")
+                                    self.p1_select_an_attack()
+                            elif attack_selection == "3":
+                                if groups[1][2].health > 0:
+                                    dinobot.attack(groups[1][2])
+                                    attack_attempt = True
+                                else:
+                                    print(f"{groups[1][2].name} is dead!")
+                                    self.p1_select_an_attack()
+                        else:
+                            print(f"{dinobot.name} {attack_succeed_or_fail}")
+                            attack_attempt = True
+                    if groups[1][0].health == 0 and groups[1][1].health == 0 and groups[1][2].health == 0:
+                        has_no_health = True
+                        continue
+                    if group == groups[1]:
+                        if attack_succeed_or_fail == "dino or bot":
+                            self.p2_select_an_attack()
+                            if attack_selection == "1":
+                                if groups[0][0].health > 0:
+                                    dinobot.attack(groups[0][0])
+                                    attack_attempt = True
+                                else:
+                                    print(f"{groups[0][0].name} is dead!")
+                                    self.p2_select_an_attack()
+                            elif attack_selection == "2":
+                                if groups[0][1].health > 0:
+                                    dinobot.attack(groups[0][1])
+                                    attack_attempt = True
+                                else:
+                                    print(f"{groups[0][1].name} is dead!")
+                                    self.p2_select_an_attack()
+                            elif attack_selection == "3":
+                                if groups[0][2].health > 0:
+                                    dinobot.attack(groups[0][2])
+                                    attack_attempt = True
+                                else:
+                                    print(f"{groups[0][2].name} is dead!")
+                        else:
+                            print(f"{dinobot.name} {attack_succeed_or_fail}")
+                        if groups[0][0].health == 0 and groups[0][1].health == 0 and groups[0][2].health == 0:
+                            self.fleet.heal
+                            attack_attempt = True
+                            continue
 
     def battle_phase(self):
-        first_attack = self.choose_team()
-        has_no_health = False
-        dino_hit_or_miss_list = ["robot", "robot", "Missed", "robot", "robot", "robot", "robot", "Missed"]
-        bot_hit_or_miss_list = ["dinosaur", "dinosaur", "Missed", "dinosaur", "dinosaur", "dinosaur", "dinosaur", "Missed"]
-        while has_no_health == False:
-            if first_attack == self.dinosaur:
-                dino_attack_succeed_or_fail = choice(dino_hit_or_miss_list)
-                if dino_attack_succeed_or_fail == "robot":
-                    self.dinosaur.attack(self.robot)
-                else:
-                    print(f"{self.dinosaur.name} {dino_attack_succeed_or_fail}")
-                if self.robot.health == 0:
-                    has_no_health = True
-                    continue
-                bot_attack_succeed_or_fail = choice(bot_hit_or_miss_list)
-                if bot_attack_succeed_or_fail == "dinosaur":
-                    self.robot.attack(self.dinosaur)
-                else:
-                    print(f"{self.robot.name} {bot_attack_succeed_or_fail}")
-                if self.dinosaur.health == 0:
-                    has_no_health = True
-                    continue
-            elif first_attack == self.robot:
-                bot_attack_succeed_or_fail = choice(bot_hit_or_miss_list)
-                if bot_attack_succeed_or_fail == "dinosaur":
-                    self.robot.attack(self.dinosaur)
-                else:
-                    print(f"{self.robot.name} {bot_attack_succeed_or_fail}")
-                if self.dinosaur.health == 0:
-                    has_no_health = True
-                    continue
-                dino_attack_succeed_or_fail = choice(dino_hit_or_miss_list)
-                if dino_attack_succeed_or_fail == "robot":
-                    self.dinosaur.attack(self.robot)
-                else:
-                    print(f"{self.dinosaur.name} {dino_attack_succeed_or_fail}")
-                if self.robot.health == 0:
-                    has_no_health = True
-                    continue
+        self.choose_enemy_hit()
 
     def display_winner(self):
-        if self.robot.health == 0:
-            print(f"{self.dinosaur.name} is the Winner!")
-        elif self.dinosaur.health == 0:
-            print(f"{self.robot.name} is the Winner!")
+        if self.fleet.health == 0:
+            print(f"{self.herd.name} is the Winner!")
+        elif self.herd.health == 0:
+            print(f"{self.fleet.name} is the Winner!")
 
     def run_game(self):
         self.display_welcome()
+        self.player_set_up()
         self.battle_phase()
         self.display_winner()
